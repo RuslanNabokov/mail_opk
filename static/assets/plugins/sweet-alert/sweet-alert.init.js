4d2958a47c645516bcf1689d366a87bcf3b8258b
ruslannabokov <musaviruslan.1996@gmail.com>
2019-08-12 17:50:33 +0300

    new burron

--- static/assets/plugins/sweet-alert/sweet-alert.init.js
+++ static/assets/plugins/sweet-alert/sweet-alert.init.js
@@ -1,102 +1,101 @@
+! function($) {
 
-!function($) {
-    
 
     var SweetAlert = function() {};
 
     //examples 
     SweetAlert.prototype.init = function() {
-    	
-    //Change Theme function
-    $('.changeTheme').click(function(){
-        var theme = $(this).find('span').html();
-			if(theme == 'default'){
-				$('head #sweetTheme').remove();
-			}else{
-				$('head #sweetTheme').remove();
-				$('head').append('<link href="assets/plugins/sweet-alert/themes/'+ theme +'/'+ theme +'.css" rel="stylesheet" id="sweetTheme">');
-			}
 
-    });
-        
-    //Basic
-    $('#sa-basic').click(function(){
-        swal("Here's a message!");
-    });
+            //Change Theme function
+            $('.changeTheme').click(function() {
+                var theme = $(this).find('span').html();
+                if (theme == 'default') {
+                    $('head #sweetTheme').remove();
+                } else {
+                    $('head #sweetTheme').remove();
+                    $('head').append('<link href="assets/plugins/sweet-alert/themes/' + theme + '/' + theme + '.css" rel="stylesheet" id="sweetTheme">');
+                }
 
-    //A title with a text under
-    $('#sa-title').click(function(){
-        swal("Here's a message!", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lorem erat, tincidunt vitae ipsum et, pellentesque maximus enim. Mauris eleifend ex semper, lobortis purus sed, pharetra felis")
-    });
+            });
 
-    //Success Message
-    $('#sa-success').click(function(){
-        swal("Good job!", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lorem erat, tincidunt vitae ipsum et, pellentesque maximus enim. Mauris eleifend ex semper, lobortis purus sed, pharetra felis", "success")
-    });
+            //Basic
+            $('#sa-basic').click(function() {
+                swal("Here's a message!");
+            });
 
-    //Warning Message
-    $('#sa-warning').click(function(){
-        swal({   
-            title: "Are you sure?",   
-            text: "You will not be able to recover this imaginary file!",   
-            type: "warning",   
-            showCancelButton: true,   
-            confirmButtonColor: "#DD6B55",   
-            confirmButtonText: "Yes, delete it!",   
-            closeOnConfirm: false 
-        }, function(){   
-            swal("Deleted!", "Your imaginary file has been deleted.", "success"); 
-        });
-    });
+            //A title with a text under
+            $('#sa-title').click(function() {
+                swal("Here's a message!", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lorem erat, tincidunt vitae ipsum et, pellentesque maximus enim. Mauris eleifend ex semper, lobortis purus sed, pharetra felis")
+            });
 
-    //Parameter
-    $('#sa-params').click(function(){
-        swal({   
-            title: "Are you sure?",   
-            text: "You will not be able to recover this imaginary file!",   
-            type: "warning",   
-            showCancelButton: true,   
-            confirmButtonColor: "#DD6B55",   
-            confirmButtonText: "Yes, delete it!",   
-            cancelButtonText: "No, cancel plx!",   
-            closeOnConfirm: false,   
-            closeOnCancel: false 
-        }, function(isConfirm){   
-            if (isConfirm) {     
-                swal("Deleted!", "Your imaginary file has been deleted.", "success");   
-            } else {     
-                swal("Cancelled", "Your imaginary file is safe :)", "error");   
-            } 
-        });
-    });
+            //Success Message
+            $('#sa-success').click(function() {
+                swal("Good job!", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lorem erat, tincidunt vitae ipsum et, pellentesque maximus enim. Mauris eleifend ex semper, lobortis purus sed, pharetra felis", "success")
+            });
 
-    //Custom Image
-    $('#sa-image').click(function(){
-        swal({   
-            title: "Sweet!",   
-            text: "Here's a custom image.",   
-            imageUrl: "assets/plugins/sweet-alert/thumbs-up.jpg" 
-        });
-    });
+            //Warning Message
+            $('#sa-warning').click(function() {
+                swal({
+                    title: "Are you sure?l",
+                    text: "You will not be able to recover this imaginary file!",
+                    type: "warning",
+                    showCancelButton: true,
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "Yes, delete it!",
+                    closeOnConfirm: false
+                }, function() {
+                    swal("Deleted!", "Your imaginary file has been deleted.", "success if (confirm('Are you sure?')){ ");
+                });
+            });
 
-    //Auto Close Timer
-    $('#sa-close').click(function(){
-         swal({   
-            title: "Auto close alert!",   
-            text: "I will close in 2 seconds.",   
-            timer: 2000,   
-            showConfirmButton: false 
-        });
-    });
+            //Parameter
+            $('#sa-params').click(function() {
+                swal({
+                    title: "Are you sure?l,",
+                    text: "You will not be able to recover this imaginary file!",
+                    type: "warning",
+                    showCancelButton: true,
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "Yes, delete it!",
+                    cancelButtonText: "No, cancel plx!",
+                    closeOnConfirm: false,
+                    closeOnCancel: false
+                }, function(isConfirm) {
+                    if (isConfirm) {
+                        swal("Deleted!", "Your imaginary file has been deleted.", "success");
+                    } else {
+                        swal("Cancelled", "Your imaginary file is safe :)", "error");
+                    }
+                });
+            });
+
+            //Custom Image
+            $('#sa-image').click(function() {
+                swal({
+                    title: "Sweet!",
+                    text: "Here's a custom image.",
+                    imageUrl: "assets/plugins/sweet-alert/thumbs-up.jpg"
+                });
+            });
+
+            //Auto Close Timer
+            $('#sa-close').click(function() {
+                swal({
+                    title: "Auto close alert!",
+                    text: "I will close in 2 seconds.",
+                    timer: 2000,
+                    showConfirmButton: false
+                });
+            });
 
 
-    },
-    //init
-    $.SweetAlert = new SweetAlert, $.SweetAlert.Constructor = SweetAlert
+        },
+        //init
+        $.SweetAlert = new SweetAlert, $.SweetAlert.Constructor = SweetAlert
 }(window.jQuery),
 
 //initializing 
 function($) {
-    
+
     $.SweetAlert.init()
 }(window.jQuery);
